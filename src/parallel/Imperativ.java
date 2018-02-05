049721dbba4e3101b30375989c91bf3dcade2093
Christoph <Christoph@DESKTOP-CP3EE7H>
2018-02-05 16:42:51 +0100

    Optimierung beider Verfahren.

--- src/parallel/Imperativ.java
+++ src/parallel/Imperativ.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package parallel;
 
 import java.awt.image.BufferedImage;
@@ -14,7 +9,7 @@ import javax.imageio.ImageIO;
  *
  * @author Christoph
  */
-public class Seriell {
+public class Imperativ {
 
     private static int[] mSource;
     private static int mStart;
@@ -22,37 +17,24 @@ public class Seriell {
     private static int[] mDestination;
 
     public static void main(String[] args) throws IOException {
-
         String srcName = "space.png";
         File srcFile = new File(srcName);
         BufferedImage image = ImageIO.read(srcFile);
-
-        System.out.println("Source image: " + srcName);
-
         BufferedImage blurredImage = gray(image);
-
         String dstName = "space-gray.png";
         File dstFile = new File(dstName);
         ImageIO.write(blurredImage, "png", dstFile);
-
-        System.out.println("Output image: " + dstName);
-
     }
 
     private static BufferedImage gray(BufferedImage srcImage) {
         int w = srcImage.getWidth();
         int h = srcImage.getHeight();
-
         int[] src = srcImage.getRGB(0, 0, w, h, null, 0, w);
         int[] dst = new int[src.length];
-        
         mSource = src;
         mStart = 0;
         mLength = src.length;
         mDestination = dst;
-
-        System.out.println("Array size is " + src.length);
-        
         long startTime = System.currentTimeMillis();
         computeDirectly();
         long endTime = System.currentTimeMillis();
@@ -61,23 +43,17 @@ public class Seriell {
         BufferedImage dstImage =
                 new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
         dstImage.setRGB(0, 0, w, h, dst, 0, w);
-
         return dstImage;
-        
-        
     }
 
     protected static void computeDirectly() {
         for (int index = mStart; index < mStart + mLength; index++) {
             int pixel = mSource[index];
-            
             float alpha = (pixel >> 24) & 0xff;
             float red = (pixel >> 16) & 0xff;
             float green = (pixel >> 8) & 0xff;
             float blue = pixel & 0xff;
             float gray = (float) ((red * 0.21) + (green * 0.72) + (blue * 0.07));
-
-            // Re-assemble destination pixel.
             int dpixel = ((int)alpha << 24)
                     | (((int) gray) << 16)
                     | (((int) gray) << 8)
@@ -85,5 +61,4 @@ public class Seriell {
             mDestination[index] = dpixel;
         }
     }
-    
 }
--- src/parallel/Parallel.java
+++ src/parallel/Parallel.java
@@ -6,6 +6,10 @@ import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
 import javax.imageio.ImageIO;
 
+/**
+ *
+ * @author Christoph
+ */
 public class Parallel extends RecursiveAction {
 
     private int[] mSource;
@@ -20,19 +24,14 @@ public class Parallel extends RecursiveAction {
         mDestination = dst;
     }
 
-    // Average pixels from source, write results into destination.
     protected void computeDirectly() {
-        
         for (int index = mStart; index < mStart + mLength; index++) {
             int pixel = mSource[index];
-            
             float alpha = (pixel >> 24) & 0xff;
             float red = (pixel >> 16) & 0xff;
             float green = (pixel >> 8) & 0xff;
             float blue = pixel & 0xff;
             float gray = (float) ((red * 0.21) + (green * 0.72) + (blue * 0.07));
-
-            // Re-assemble destination pixel.
             int dpixel = ((int)alpha << 24)
                     | (((int) gray) << 16)
                     | (((int) gray) << 8)
@@ -40,7 +39,7 @@ public class Parallel extends RecursiveAction {
             mDestination[index] = dpixel;
         }
     }
-    protected static int sThreshold = 10000;
+    protected static int sThreshold = 1000;
 
     @Override
     protected void compute() {
@@ -48,62 +47,37 @@ public class Parallel extends RecursiveAction {
             computeDirectly();
             return;
         }
-
         int split = mLength / 2;
-
         invokeAll(new Parallel(mSource, mStart, split, mDestination),
                 new Parallel(mSource, mStart + split, mLength - split, 
                 mDestination));
     }
 
-    // Plumbing follows.
     public static void main(String[] args) throws Exception {
         String srcName = "space.png";
         File srcFile = new File(srcName);
-        BufferedImage image = ImageIO.read(srcFile);
-        
-        System.out.println("Source image: " + srcName);
-        
+        BufferedImage image = ImageIO.read(srcFile);  
         BufferedImage blurredImage = gray(image);
-        
         String dstName = "space-gray.png";
         File dstFile = new File(dstName);
         ImageIO.write(blurredImage, "png", dstFile);
-        
-        System.out.println("Output image: " + dstName);
-        
     }
 
     public static BufferedImage gray(BufferedImage srcImage) {
         int w = srcImage.getWidth();
         int h = srcImage.getHeight();
-
         int[] src = srcImage.getRGB(0, 0, w, h, null, 0, w);
         int[] dst = new int[src.length];
-
-        System.out.println("Array size is " + src.length);
-        System.out.println("Threshold is " + sThreshold);
-
-        int processors = Runtime.getRuntime().availableProcessors();
-        System.out.println(Integer.toString(processors) + " processor"
-                + (processors != 1 ? "s are " : " is ")
-                + "available");
-
         Parallel fb = new Parallel(src, 0, src.length, dst);
-
         ForkJoinPool pool = new ForkJoinPool();
-
         long startTime = System.currentTimeMillis();
         pool.invoke(fb);
         long endTime = System.currentTimeMillis();
-
         System.out.println("Image blur took " + (endTime - startTime) + 
                 " milliseconds.");
-
         BufferedImage dstImage =
                 new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
         dstImage.setRGB(0, 0, w, h, dst, 0, w);
-
         return dstImage;
     }
 }
